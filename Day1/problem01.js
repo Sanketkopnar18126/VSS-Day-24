3a19538e6cd9020a64250b3b2982d08124a48d04
sanket <sanketkopnar1299@gmail.com>
2023-08-08 18:42:03 +0530

    first program

---
+++ Day1/problem01.js
@@ -0,0 +1,18 @@
+let str = "bob";
+
+// console.log({ newstr });
+let remodub = (n) => {
+  let newstr = n.split("");
+  console.log("newn", newstr);
+
+  for (let i = 0; i < newstr.length; i++) {
+    for (let j = i + 1; j < newstr.length; j++) {
+      if (n[i] === n[j]) {
+        newstr.splice(j, 1);
+        return newstr;
+      }
+    }
+  }
+};
+
+console.log(remodub(str));
